917fea965408d37787d7d3f8cc5518c42ef16edd
Backtun <backtunn@gmail.com>
2018-02-03 01:36:00 -0600

    Se crea clase de conexión y función para validar los datos del usuario en el login.

---
+++ Facturacion/Conexion.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Conexion
+    '? Método autenticación por sql server
+    'Public cs_conexion As String = "data source = DESKTOP-3NCIUST\SQLEXPRESS; initial catalog = sis_factura; user id = ; password = "
+    '? Método de autenticación por windows
+    Public cs_conexion As String = "Data Source = DESKTOP-3NCIUST\SQLEXPRESS; Initial Catalog = sis_factura; Integrated Security = True"
+    Private cn As New SqlConnection(cs_conexion)
+
+    ''' <summary>
+    ''' Abrir conexión a la base de datos
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function Conectarse() As Boolean
+        Dim estado As Boolean
+        Try
+            cn.Open()
+            estado = True
+        Catch ex As Exception
+            estado = False
+        End Try
+        Return estado
+    End Function
+
+    ''' <summary>
+    ''' Cerrar conexión
+    ''' </summary>
+    Public Sub Desconectar()
+        cn.Close()
+    End Sub
+
+    ''' <summary>
+    ''' Validar si existe usuario en la base de datos
+    ''' </summary>
+    ''' <param name="usuario"></param>
+    ''' <param name="clave"></param>
+    ''' <returns></returns>
+    Public Function ValidarUsuario(ByVal usuario As String, ByVal clave As String) As DataTable
+        Dim tbdatos As New DataTable
+        Dim adaptador As New SqlDataAdapter
+        '? Nombre del procedimiento almacenado "ValidarUsuario"
+        Dim comando As New SqlCommand("ValidarUsuario", cn)
+
+        If Conectarse() Then
+
+            With comando
+                ' Tipo, procedimiento almacenado
+                .CommandType = CommandType.StoredProcedure
+                .Parameters.AddWithValue("@pusuario", usuario)
+                .Parameters.AddWithValue("@pclave", clave)
+            End With
+
+            Try
+                adaptador.SelectCommand = comando
+                adaptador.Fill(tbdatos)
+            Catch ex As Exception
+                Return Nothing
+            End Try
+
+            Desconectar()
+        End If
+
+        Return tbdatos
+    End Function
+End Class
--- Facturacion/Login.aspx.vb
+++ Facturacion/Login.aspx.vb
@@ -1,13 +1,25 @@
 ﻿Public Class Login
     Inherits System.Web.UI.Page
 
+    Dim conexion As New Conexion
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+    ''' <summary>
+    ''' Evento para el botón ingresar
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Protected Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
-        'conectar a la base
-        'verificar si existe el usuario
-        'redireccionar
+        Dim tabla As New DataTable
+        tabla = conexion.ValidarUsuario(txtUsuario.Text, txtPassword.Text)
+
+        If tabla.Rows.Count() = 0 Then
+            Response.Write("Usario no válido")
+        Else
+            Response.Write("Usario válido")
+        End If
     End Sub
 End Class
